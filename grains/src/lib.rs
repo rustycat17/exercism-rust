3a6e5e1c2f39a75730ab1265bc3dc1bb72da7cd2
rustycat17 <rustycat17@posteo.nz>
2021-03-14 13:48:46 +0100

    added grains algorithm

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,22 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    let mut result = 1;
+
+    if !(1..=64).contains(&s) {
+        panic!("Square must be between 1 and 64");
+    }
+
+    for _ in 1..s {
+        result *= 2;
+    }
+
+    result
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let mut result = 0;
+    for i in 1..65 {
+        result += square(i);
+    }
+
+    result
 }
