f534bdad657a69ccb58a3fad69ebb36eb83d727a
rustycat17 <rustycat17@posteo.nz>
2021-03-14 11:21:16 +0100

    added function build_proverb()

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+
+    let mut res: Vec<String> = vec![];
+
+    for i in 0..list.len() - 1 {
+        res.push(format!(
+            "For want of a {0} the {1} was lost.",
+            list[i],
+            list[i + 1]
+        ));
+    }
+
+    res.push(format!("And all for the want of a {}.", list[0]));
+
+    res.join("\n")
 }
