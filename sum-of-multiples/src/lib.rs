9b4a70b9fdcff38d19ed6e0be4af0a71299ccf69
rustycat17 <rustycat17@posteo.nz>
2021-03-14 13:32:44 +0100

    added sum-of-multiples algorithm

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut res_vec = vec![];
+    let mut result = 0;
+
+    for i in 1..limit {
+        for k in factors {
+            if *k > 0 {
+                if i % k == 0 && !res_vec.contains(&i) {
+                    res_vec.push(i);
+                }
+            }
+        }
+    }
+
+    for element in res_vec {
+        result += element;
+    }
+
+    result
 }
