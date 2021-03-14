9d9f2a9a9afb0758bb8ed0d2979874731925c038
rustycat17 <rustycat17@posteo.nz>
2021-03-14 09:39:39 +0100

    added nth function to nth-prime project

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes = vec![2];
+
+    let mut current_number = primes[0];
+    loop {
+        if primes.len() as u32 - 1 == n {
+            break;
+        }
+        current_number += 1;
+        let mut is_prime = true;
+        'inner: for current_prime in primes.iter() {
+            if current_number % current_prime == 0 {
+                is_prime = false;
+                break 'inner;
+            }
+        }
+
+        if is_prime {
+            primes.push(current_number);
+        }
+    }
+
+    *primes.last().unwrap()
 }
