ff2a5c7cd8b3c5f0a386ce5d316919ffd240a8ec
rustycat17 <rustycat17@posteo.nz>
2021-03-14 14:47:38 +0100

    added functionality for reverse-string

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,7 @@
+extern crate unicode_segmentation;
+
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
