734a779e3cda1c3e2c9c2c39ed6d1526f65fa3c3
rustycat17 <rustycat17@posteo.nz>
2021-03-15 19:21:34 +0100

    added algorithm for matching-brackets problem

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,31 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = vec![];
+    for letter in string.chars() {
+        match letter {
+            '(' | '[' | '{' => stack.push(letter),
+            ')' => {
+                if stack.last() == Some(&'(') {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            }
+            ']' => {
+                if stack.last() == Some(&'[') {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            }
+            '}' => {
+                if stack.last() == Some(&'{') {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            }
+            _ => {}
+        };
+    }
+    stack.is_empty()
 }
