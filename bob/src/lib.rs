247d69ea247d40730cc6a5c5bbd53f808852b228
rustycat17 <rustycat17@posteo.nz>
2021-03-15 19:13:48 +0100

    added implementation for bob project

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    if message.trim_end().ends_with('?')
+        && message.to_uppercase() == message
+        && message.chars().any(char::is_alphabetic)
+    {
+        "Calm down, I know what I'm doing!"
+    } else if message.to_uppercase() == message
+        && !message.trim_end().is_empty()
+        && message.chars().any(char::is_alphabetic)
+    {
+        "Whoa, chill out!"
+    } else if message.trim_end().ends_with('?') {
+        "Sure."
+    } else if message.trim_end().is_empty() {
+        "Fine. Be that way!"
+    } else {
+        "Whatever."
+    }
 }
